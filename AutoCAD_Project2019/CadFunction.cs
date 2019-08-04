41eca9d21feb1e02c0b16c0fb72fffef4ffb0acc
Wenjun Sang <SESA242524@schneider-electric.com>
2019-08-04 14:29:31 -0400

    Add project files.

---
+++ AutoCAD_Project2019/CadFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+using AcAp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace AutoCAD_Project2019
+{
+       public class Commands
+    {
+        [CommandMethod("TEST")]
+        public void Test()
+        {
+            var doc = AcAp.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            var ed = doc.Editor;
+            using (var tr = db.TransactionManager.StartTransaction())
+            {
+                tr.Commit();
+            }
+        }
+    }
+}
