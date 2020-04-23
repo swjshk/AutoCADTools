42e585f49727b38d3bb250c7b6663d134782880b
Max Sang <wenjunsang@gmail.com>
2020-04-22 23:10:16 -0400

    Opendrawing()

--- AutoCAD_Project2019/CadFunction.cs
+++ AutoCAD_Project2019/CadFunction.cs
@@ -8,6 +8,8 @@ using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
+using System.IO;
+using System.Windows.Forms;
 
 using AcAp = Autodesk.AutoCAD.ApplicationServices.Application;
 
@@ -15,8 +17,8 @@ namespace AutoCAD_Project2019
 {
        public class Commands
     {
-        [CommandMethod("TEST")]
-        public void Test()
+        [CommandMethod("TEST12")]
+        public void test12()
         {
             var doc = AcAp.DocumentManager.MdiActiveDocument;
             var db = doc.Database;
@@ -25,6 +27,28 @@ namespace AutoCAD_Project2019
             {
                 tr.Commit();
             }
+        }
+
+        [CommandMethod("Opendrawing", CommandFlags.Session) ]
+        public void OpenDrawing()
+        {
+            var filePath=string.Empty;
+            using (OpenFileDialog openfiledialog1= new OpenFileDialog())
+            {
+                openfiledialog1.InitialDirectory = "C:\\FO\\";
+                openfiledialog1.Filter = "dwg files(*.dwg)|*.dwg";
+                if (openfiledialog1.ShowDialog()==DialogResult.OK)
+                {
+                    filePath = openfiledialog1.FileName;
+                }
+              
+            }
+            DocumentCollection acDocMgr = AcAp.DocumentManager;
+            acDocMgr.Open(filePath,false);
+            var docName=acDocMgr.MdiActiveDocument.Name;
+            //acDocMgr.Open(filePath, false);
+            acDocMgr.Add("drawing1.dwg");
+           
         }
     }
 }
