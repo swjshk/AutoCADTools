cd6d6f76bbfed254bd8d700f96fd11f6a6a0c0f0
Max Sang <wenjunsang@gmail.com>
2020-04-23 00:51:39 -0400

    add design diagram

--- AutoCAD_Project2019/CadFunction.cs
+++ AutoCAD_Project2019/CadFunction.cs
@@ -32,23 +32,23 @@ namespace AutoCAD_Project2019
         [CommandMethod("Opendrawing", CommandFlags.Session) ]
         public void OpenDrawing()
         {
-            var filePath=string.Empty;
-            using (OpenFileDialog openfiledialog1= new OpenFileDialog())
+            var filePath = string.Empty;
+            using (OpenFileDialog openfiledialog1 = new OpenFileDialog())
             {
                 openfiledialog1.InitialDirectory = "C:\\FO\\";
                 openfiledialog1.Filter = "dwg files(*.dwg)|*.dwg";
-                if (openfiledialog1.ShowDialog()==DialogResult.OK)
+                if (openfiledialog1.ShowDialog() == DialogResult.OK)
                 {
                     filePath = openfiledialog1.FileName;
                 }
-              
+
             }
             DocumentCollection acDocMgr = AcAp.DocumentManager;
-            acDocMgr.Open(filePath,false);
-            var docName=acDocMgr.MdiActiveDocument.Name;
+            acDocMgr.Open(filePath, false);
+            var docName = acDocMgr.MdiActiveDocument.Name;
             //acDocMgr.Open(filePath, false);
             acDocMgr.Add("drawing1.dwg");
-           
+
         }
     }
 }
